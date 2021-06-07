98b44abec9a828451a866fde8ee70e3684ffd99f
stephenjreid <sreid@gwlab.com.au>
2021-06-07 22:34:17 +1000

    Test

--- src/WiiseConnector.php
+++ src/WiiseConnector.php
@@ -13,12 +13,23 @@ class WiiseConnector
         $accessToken = $tokenCache->getAccessToken('wiise');
 
         $url = config('wiiseConnector.baseUrl').config('wiiseConnector.tennantId')."/Production/ODataV4/Company('".config('wiiseConnector.companyName')."')/Job_List?\$select=No,Description,Bill_to_Customer_No,Status,Person_Responsible,Search_Description,Project_Manager";
-        
-        $request = Request::create($url,'GET');
-        $request->headers->set('Accept', 'application/json');
-        $request->headers->set('Authorization', 'Bearer '.$accessToken);
-        $res = app()->handle($request);
-        
-        dd($res);
+        $options = [];
+
+        $provider = new \League\OAuth2\Client\Provider\GenericProvider();
+
+        try {
+            $request = $provider->getAuthenticatedRequest(
+            'GET',
+            $url,
+            $accessToken,
+            $options
+            );
+          //parse response
+          $response = $provider->getResponse($request);
+          $result = $contents = json_decode($response->getBody()->getContents());
+          dd($result);
+          } catch (Exception $ex) {
+            dd($ex);
+        }
     }
 }
