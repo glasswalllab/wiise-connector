6543f0095e9b3c31642799f131e49d21a7e2c475
stephenjreid <sreid@gwlab.com.au>
2021-06-07 22:23:58 +1000

    Test

--- src/WiiseConnector.php
+++ src/WiiseConnector.php
@@ -2,11 +2,22 @@
 
 namespace Glasswalllab\WiiseConnector;
 
+use Illuminate\Http\Request;
+
 class WiiseConnector
 {
     public function getjobs()
     {
+        $tokenCache = new TokenCache();
+        $accessToken = $tokenCache->getAccessToken('wiise');
+
         $url = config('wiiseConnector.baseUrl').config('wiiseConnector.tennantId')."/Production/ODataV4/Company('".config('wiiseConnector.companyName')."')/Job_List?\$select=No,Description,Bill_to_Customer_No,Status,Person_Responsible,Search_Description,Project_Manager";
-        dd($url);
+        
+        $request = Request::create($url,'GET');
+        $request->headers->set('Accept', 'application/json');
+        $request->headers->set('Authorization', 'Bearer '.$accessToken);
+        $res = app()->handle($request);
+        
+        dd($res);
     }
 }
