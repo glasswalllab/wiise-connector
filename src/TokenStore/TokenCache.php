ee1017381c4fa7d086e7531688de66d87718e585
stephenjreid <sreid@gwlab.com.au>
2021-06-08 09:21:29 +1000

    Fix refresh token

--- src/TokenStore/TokenCache.php
+++ src/TokenStore/TokenCache.php
@@ -52,7 +52,6 @@ class TokenCache {
         'urlResourceOwnerDetails' => '',
       ]);
   
-      dd($token->refreshToken);
       try {
         $newToken = $oauthClient->getAccessToken('refresh_token', [
           'refresh_token' => $token->refreshToken
