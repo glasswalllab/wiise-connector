736a14622fa2b6fb5f60138b070ce9cd43d50c87
stephenjreid <sreid@gwlab.com.au>
2021-06-07 22:49:09 +1000

    Update WiiseConnector with methods

--- src/WiiseConnector.php
+++ src/WiiseConnector.php
@@ -8,12 +8,20 @@ use Illuminate\Http\Request;
 class WiiseConnector
 {
     public function getjobs()
+    {  
+        $result = callwebservice('/Job_List?\$select=No,Description,Bill_to_Customer_No,Status,Person_Responsible,Search_Description,Project_Manager','GET','');
+        return ($result);
+    }
+
+    private function callwebservice($endpoint,$method,$body)
     {
         $tokenCache = new TokenCache();
         $accessToken = $tokenCache->getAccessToken('wiise');
 
-        $url = config('wiiseConnector.baseUrl').config('wiiseConnector.tennantId')."/Production/ODataV4/Company('".config('wiiseConnector.companyName')."')/Job_List?\$select=No,Description,Bill_to_Customer_No,Status,Person_Responsible,Search_Description,Project_Manager";
-        $options = [];
+        $url = config('wiiseConnector.baseUrl').config('wiiseConnector.tennantId')."/Production/ODataV4/Company('".config('wiiseConnector.companyName')."')".$endpoint;
+
+        $options['headers']['content-type'] = 'application/json';
+        $options['body'] = $body; //json encoded value
 
         $provider = new \League\OAuth2\Client\Provider\GenericProvider([
             'clientId'                => config('wiiseConnector.appId'),
@@ -24,19 +32,23 @@ class WiiseConnector
             'urlResourceOwnerDetails' => '',
           ]);
 
-        try {
+        try
+        {
             $request = $provider->getAuthenticatedRequest(
-            'GET',
-            $url,
-            $accessToken,
-            $options
+                $method,
+                $url,
+                $accessToken,
+                $options,
             );
-          //parse response
-          $response = $provider->getResponse($request);
-          $result = $contents = json_decode($response->getBody()->getContents());
-          dd($result);
-          } catch (Exception $ex) {
-            dd($ex);
+
+            //parse response
+            $response = $provider->getResponse($request);
+            $result = $contents = json_decode($response->getBody()->getContents());
+            
+            return($result);
+
+        } catch (Exception $ex) {
+            return($ex);
         }
     }
 }
