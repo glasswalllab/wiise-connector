834be121fd8d553f820edeec2f83cac1ba7d749f
stephenjreid <sreid@gwlab.com.au>
2021-06-07 11:30:07 +1000

    Update route file with namespace

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use glasswalllab\wiise-connector\Http\Controllers\AuthController;
 
 Route::get('/signin', 'AuthController@signin');
 Route::get('/callback', 'AuthController@callback');
