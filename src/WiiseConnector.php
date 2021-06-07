f9369f763442744262ffd19cac03f1356df57821
stephenjreid <sreid@gwlab.com.au>
2021-06-07 22:08:30 +1000

    typeo

--- src/WiiseConnector.php
+++ src/WiiseConnector.php
@@ -6,7 +6,7 @@ class WiiseConnector
 {
     public function getjobs()
     {
-        $url = config('wiiseConnector.baseUrl').config('wiiseConnector.tennantId')."Production/ODataV4/Company(".config('wiiseConnector.companyName')."/Job_List?\$select=No,Description,Bill_to_Customer_No,Status,Person_Responsible,Search_Description,Project_Manager";
+        $url = config('wiiseConnector.baseUrl').config('wiiseConnector.tennantId')."/Production/ODataV4/Company('".config('wiiseConnector.companyName')."')/Job_List?\$select=No,Description,Bill_to_Customer_No,Status,Person_Responsible,Search_Description,Project_Manager";
         dd($url);
     }
 }
