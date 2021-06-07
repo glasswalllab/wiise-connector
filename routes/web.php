493bb94c4f2e1414148d0f694ac6a04b932065da
stephenjreid <sreid@gwlab.com.au>
2021-06-07 11:52:32 +1000

    Rename wiiseConnector to wiise_connector

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use glasswalllab\wiiseConnector\Http\Controllers\AuthController;
+use glasswalllab\wiise_connector\Http\Controllers\AuthController;
 
 Route::get('/signin', [AuthController::class, 'signin']);
 Route::get('/callback', [AuthController::class, 'callback']);
--- src/WiiseConnectorServiceProvider.php
+++ src/WiiseConnectorServiceProvider.php
@@ -14,29 +14,29 @@ class WiiseConnectorServiceProvider extends ServiceProvider
         /*
          * Optional methods to load your package assets
          */
-        // $this->loadTranslationsFrom(__DIR__.'/../resources/lang', 'wiise-connector');
-        // $this->loadViewsFrom(__DIR__.'/../resources/views', 'wiise-connector');
+        // $this->loadTranslationsFrom(__DIR__.'/../resources/lang', 'wiiseConnector');
+        // $this->loadViewsFrom(__DIR__.'/../resources/views', 'wiiseConnector');
         // $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
         $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
 
         if ($this->app->runningInConsole()) {
             $this->publishes([
-                __DIR__.'/../config/config.php' => config_path('wiise-connector.php'),
+                __DIR__.'/../config/config.php' => config_path('wiiseConnector.php'),
             ], 'config');
 
             // Publishing the views.
             /*$this->publishes([
-                __DIR__.'/../resources/views' => resource_path('views/vendor/wiise-connector'),
+                __DIR__.'/../resources/views' => resource_path('views/vendor/wiiseConnector'),
             ], 'views');*/
 
             // Publishing assets.
             /*$this->publishes([
-                __DIR__.'/../resources/assets' => public_path('vendor/wiise-connector'),
+                __DIR__.'/../resources/assets' => public_path('vendor/wiiseConnector'),
             ], 'assets');*/
 
             // Publishing the translation files.
             /*$this->publishes([
-                __DIR__.'/../resources/lang' => resource_path('lang/vendor/wiise-connector'),
+                __DIR__.'/../resources/lang' => resource_path('lang/vendor/wiiseConnector'),
             ], 'lang');*/
 
             // Registering package commands.
@@ -50,10 +50,10 @@ class WiiseConnectorServiceProvider extends ServiceProvider
     public function register()
     {
         // Automatically apply the package configuration
-        $this->mergeConfigFrom(__DIR__.'/../config/config.php', 'wiise-connector');
+        $this->mergeConfigFrom(__DIR__.'/../config/config.php', 'wiiseConnector');
 
         // Register the main class to use with the facade
-        $this->app->singleton('wiise-connector', function () {
+        $this->app->singleton('wiiseConnector', function () {
             return new WiiseConnector;
         });
     }
