47db949326c7fcba4c4543c5b84c18f2eb855066
stephenjreid <sreid@gwlab.com.au>
2021-06-09 15:24:23 +1000

    Rename updateJobs

--- src/WiiseConnector.php
+++ src/WiiseConnector.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 
 class WiiseConnector
 {
-    public function getJobs()
+    public function updateJobs()
     {  
         $call = CallWebService::dispatchSync('/Job_List?\$select=No,Description,Bill_to_Customer_No,Status,Person_Responsible,Search_Description,Project_Manager','GET','');
         dd($call);
