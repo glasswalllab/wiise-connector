67216bf62f52b6e4b51b17c08d8800fb985907b9
stephenjreid <sreid@gwlab.com.au>
2021-06-10 08:17:30 +1000

    Trying json response

--- src/Jobs/CallWebService.php
+++ src/Jobs/CallWebService.php
@@ -17,6 +17,7 @@ class CallWebService implements ShouldQueue
     private $endpoint;
     private $method;
     private $body;
+    private $authClient;
 
     public function __construct($endpoint,$method,$body)
     {
@@ -37,7 +38,7 @@ class CallWebService implements ShouldQueue
 
         $options['body'] = $this->body; //json encoded value
         
-        $oauthClient = new \League\OAuth2\Client\Provider\GenericProvider([
+        $this->oauthClient = new \League\OAuth2\Client\Provider\GenericProvider([
             'clientId'                => config('wiiseConnector.appId'),
             'clientSecret'            => config('wiiseConnector.appSecret'),
             'redirectUri'             => config('wiiseConnector.redirectUri'),
@@ -49,7 +50,7 @@ class CallWebService implements ShouldQueue
 
         try
         {
-            $request = $oauthClient->getAuthenticatedRequest(
+            $request = $this->oauthClient->getAuthenticatedRequest(
                 $this->method,
                 $url,
                 $accessToken,
