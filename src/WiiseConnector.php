71c7ff5da79f0b34a8bb7e04a1145fecca82ce9e
stephenjreid <sreid@gwlab.com.au>
2021-06-15 18:09:32 +1000

    Update OAuth

--- src/WiiseConnector.php
+++ src/WiiseConnector.php
@@ -12,7 +12,7 @@ class WiiseConnector
     {  
         //Could move the below to job - but was having issues with the return
         $tokenCache = new TokenCache();
-        $accessToken = $tokenCache->getAccessToken(config('wiiseConnector.provider'));
+        $accessToken = $tokenCache->getAccessToken(config('Connector.provider'));
 
         $url = config('wiiseConnector.baseUrl').config('wiiseConnector.tenantId')."/Production/ODataV4/Company('".config('wiiseConnector.companyName')."')".$endpoint;
 
