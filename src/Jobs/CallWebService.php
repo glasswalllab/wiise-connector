3af76a95e26c00f18febc09b02c312942804ffe5
stephenjreid <sreid@gwlab.com.au>
2021-06-25 10:35:38 +1000

    Debug

--- src/Jobs/CallWebService.php
+++ src/Jobs/CallWebService.php
@@ -33,6 +33,8 @@ class CallWebService implements ShouldQueue
 
         $url = config('wiiseConnector.baseUrl').config('wiiseConnector.tenantId')."/Production/ODataV4/Company('".config('wiiseConnector.companyName')."')".$this->endpoint;
 
+        dd($url);
+
         $options['headers']['Content-Type'] = 'application/json';
         $options['headers']['If-Match'] = '*';
 
